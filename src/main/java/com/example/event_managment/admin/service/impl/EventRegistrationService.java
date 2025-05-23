4916cd92484d102bd31985ec3f7513c0d93235ee
Prashant Yadav <prashant1.yadav@tmf.co.in>
2025-05-23 20:07:00 +0530

    update entity files path

---
+++ src/main/java/com/example/event_managment/admin/service/impl/EventRegistrationService.java
@@ -0,0 +1,4 @@
+package com.example.event_managment.admin.service.impl;
+
+public class EventRegistrationService {
+}
---
+++ src/main/java/com/example/event_managment/common/entity/EventRegistration.java
@@ -0,0 +1,10 @@
+package com.example.event_managment.admin.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "event_registration")
+public class EventRegistration {
+}
