1b3e2a8e604c1c4eb3c48b22b929471cf1e32b5b
Prashant Yadav <py.dim123@gmail.com>
2025-12-25 18:10:05 +0530

    update code

--- src/main/java/com/example/event_management/web/controllers/WeddingRegistrationController.java
+++ src/main/java/com/example/event_management/web/controllers/WeddingRegistrationController.java
@@ -17,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/admin/wedding/registration/")
+@RequestMapping("/api/wedding/registration/")
 @RequiredArgsConstructor
 public class WeddingRegistrationController {
 
